f5796189e149abf2b561e1a8cf372b67c7303d6d
Bekjan1337 <167067494+Bekjan1337@users.noreply.github.com>
2024-06-29 01:50:05 +0600

    Update Detail.jsx

--- src/components/products/Detail.jsx
+++ src/components/products/Detail.jsx
@@ -1,18 +1,53 @@
 import React from "react";
-import { Box, Modal, Typography, Grid } from "@mui/material";
+import {
+  Box,
+  Modal,
+  Typography,
+  Paper,
+  IconButton,
+  Avatar,
+} from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { useProduct } from "../../context/ProductContextProvider";
 
 const Detail = ({ elem, open, handleClose }) => {
+  const { deleteComment } = useProduct();
+
+  const handleDeleteComment = (commentIndex) => {
+    deleteComment(elem.id, commentIndex);
+  };
+
   const modalStyle = {
     position: "absolute",
     top: "50%",
     left: "50%",
     transform: "translate(-50%, -50%)",
-    width: "80%",
-    maxWidth: 700,
+    width: "90%",
+    maxWidth: 1000,
     bgcolor: "white",
     boxShadow: 24,
     borderRadius: 8,
     p: 4,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "stretch",
+  };
+
+  const contentStyle = {
+    width: "60%",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+  };
+
+  const commentsStyle = {
+    width: "35%",
+    marginLeft: "5%",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
   };
 
   const imageContainerStyle = {
@@ -26,6 +61,8 @@ const Detail = ({ elem, open, handleClose }) => {
     height: "auto",
     borderRadius: 8,
     boxShadow: "0 8px 16px rgba(0, 0, 0, 0.1)",
+    maxHeight: "200px",
+    objectFit: "cover",
   };
 
   const textContainerStyle = {
@@ -33,30 +70,80 @@ const Detail = ({ elem, open, handleClose }) => {
     flexDirection: "column",
     alignItems: "center",
     textAlign: "center",
+    marginBottom: 16,
   };
 
-  const descriptionStyle = {
-    marginBottom: 8,
-  };
+  const descriptionStyle = { marginBottom: 8 };
 
   return (
     <Modal open={open} onClose={handleClose}>
       <Box sx={modalStyle}>
-        <Typography variant="h5" gutterBottom align="center">
-          {elem.country}
-        </Typography>
-        <div style={imageContainerStyle}>
-          <img src={elem.image1} alt="" style={imageStyle} />
-          <img src={elem.image2} alt="" style={imageStyle} />
-        </div>
-        <div style={textContainerStyle}>
-          <Typography variant="body1" style={descriptionStyle}>
-            {elem.description}
+        <Box sx={contentStyle}>
+          <Typography variant="h5" gutterBottom align="center">
+            {elem.country}
           </Typography>
-          <Typography variant="body1" color="textSecondary">
-            Price: {elem.price}
+          <div style={imageContainerStyle}>
+            <img
+              src={elem.image1}
+              alt={`Image of ${elem.country}`}
+              style={imageStyle}
+            />
+            <img
+              src={elem.image2}
+              alt={`Image of ${elem.country}`}
+              style={imageStyle}
+            />
+          </div>
+          <div style={textContainerStyle}>
+            <Typography variant="body1" style={descriptionStyle}>
+              {elem.description}
+            </Typography>
+            <Typography variant="body1" color="textSecondary">
+              Price: {elem.price}
+            </Typography>
+          </div>
+        </Box>
+        <Box sx={commentsStyle}>
+          <Typography variant="subtitle1" align="center">
+            Comments:
           </Typography>
-        </div>
+          {elem.comments && elem.comments.length > 0 ? (
+            elem.comments.map((comment, index) => (
+              <Paper
+                key={index}
+                sx={{
+                  padding: 1,
+                  marginBottom: 1,
+                  border:
+                    comment.name === "Author"
+                      ? "1px solid blue"
+                      : "1px solid gray",
+                  width: "100%",
+                  display: "flex",
+                  alignItems: "center",
+                  position: "relative",
+                  wordBreak: "break-word",
+                }}
+              >
+                <Avatar sx={{ marginRight: 1 }} />
+                <Box sx={{ flexGrow: 1 }}>
+                  <Typography variant="subtitle2">{comment.name}</Typography>
+                  <Typography variant="body2">{comment.comment}</Typography>
+                </Box>
+                <IconButton
+                  sx={{ position: "absolute", top: 0, right: 0, p: 0.5 }}
+                  onClick={() => handleDeleteComment(index)}
+                >
+                  <DeleteIcon fontSize="small" />
+                </IconButton>
+              </Paper>
+            ))
+          ) : (
+            <Typography variant="body2" align="center">
+              No comments yet
+            </Typography>
+          )}
+        </Box>
       </Box>
     </Modal>
   );
