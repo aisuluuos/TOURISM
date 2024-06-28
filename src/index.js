38d483a95b9315dd1c9dfec98f227bd5dcb7e085
Bekjan1337 <167067494+Bekjan1337@users.noreply.github.com>
2024-06-28 15:03:34 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import ProductContextProvider from "./context/ProductContextProvider"; // Ensure the path is correct
+import ProductContextProvider from "./context/ProductContextProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
