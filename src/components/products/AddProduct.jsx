3173bf0290cd13f57aa00b79b2d21e6ce67ce61d
Aisuluu Osmonova <aisuluuosmonova6@gmail.com>
2024-06-28 23:21:23 +0600

    add

--- src/components/products/AddProduct.jsx
+++ src/components/products/AddProduct.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Box, Button, TextField, Typography, Grid } from "@mui/material";
 import { useProduct } from "../../context/ProductContextProvider";
-import CategorySelect from "./CategorySelect"; // Import CategorySelect component
+import AddCategory from "./AddCategory";
 
 const AddProduct = () => {
   const { createProduct } = useProduct();
@@ -113,49 +113,11 @@ const AddProduct = () => {
       <Button
         onClick={handleClick}
         fullWidth
-<<<<<<< HEAD
         variant="contained"
         color="primary"
         sx={{ mt: 3 }}
       >
         Add Post
-=======
-        label="Image1"
-        variant="outlined"
-        onChange={handleInput}
-      />
-      <TextField
-        name="image2"
-        fullWidth
-        label="Image2"
-        variant="outlined"
-        onChange={handleInput}
-      />
-      <TextField
-        name="country"
-        fullWidth
-        label="Country"
-        variant="outlined"
-        onChange={handleInput}
-      />
-      <TextField
-        name="description"
-        fullWidth
-        label="Description"
-        variant="outlined"
-        onChange={handleInput}
-      />
-      <TextField
-        name="price"
-        fullWidth
-        label="Price"
-        variant="outlined"
-        onChange={handleInput}
-      />
-      <CategorySelect handleInput={handleInput} />
-      <Button onClick={handleClick} fullWidth variant="contained">
-        Add Product
->>>>>>> cd80cc7fad851dd3f7ccaa1450a7d2d9d6b41f3c
       </Button>
       <AddCategory />
     </Box>
@@ -163,104 +125,3 @@ const AddProduct = () => {
 };
 
 export default AddProduct;
-
-// import { Box, Button, TextField, Typography } from "@mui/material";
-// import React, { useState } from "react";
-// import { useProduct } from "../../context/ProductContextProvider";
-// import AddCategory from "./AddCategory";
-
-// const AddProduct = () => {
-//   const { createProduct } = useProduct();
-//   const [product, setProduct] = useState({
-//     image1: "",
-//     image2: "",
-//     country: "",
-//     description: "",
-//     price: 0,
-//     category: "",
-//   });
-
-//   const handleInput = (e) => {
-//     const { name, value } = e.target;
-//     setProduct((prevProduct) => ({
-//       ...prevProduct,
-//       [name]: name === "price" ? Number(value) : value,
-//     }));
-//   };
-
-//   const handleClick = () => {
-//     console.log("Product to be created:", product);
-//     createProduct(product)
-//       .then((response) => {
-//         console.log("Product created successfully:", response);
-//       })
-//       .catch((error) => {
-//         console.error("Error creating product:", error);
-//       });
-//   };
-
-//   return (
-//     <Box
-//       sx={{
-//         width: "50vw",
-//         height: 500,
-//         margin: "20px auto",
-//         display: "flex",
-//         flexDirection: "column",
-//         justifyContent: "space-between",
-//       }}
-//     >
-//       <Typography variant="h4" align="center">
-//         ADMIN PAGE
-//       </Typography>
-//       <TextField
-//         name="image1"
-//         fullWidth
-//         label="Image1"
-//         variant="outlined"
-//         onChange={handleInput}
-//       />
-//       <TextField
-//         name="image2"
-//         fullWidth
-//         label="Image2"
-//         variant="outlined"
-//         onChange={handleInput}
-//       />
-//       <TextField
-//         name="country"
-//         fullWidth
-//         label="Country"
-//         variant="outlined"
-//         onChange={handleInput}
-//       />
-//       <TextField
-//         name="description"
-//         fullWidth
-//         label="Description"
-//         variant="outlined"
-//         onChange={handleInput}
-//       />
-//       <TextField
-//         name="price"
-//         fullWidth
-//         label="Price"
-//         variant="outlined"
-//         onChange={handleInput}
-//       />
-//       <TextField
-//         name="category"
-//         fullWidth
-//         label="Category"
-//         variant="outlined"
-//         onChange={handleInput}
-//       />
-//       <AddCategory />
-//       <Button onClick={handleClick} fullWidth variant="contained">
-//         Add Post
-//       </Button>
-//     </Box>
-//   );
-// };
-
-// export default AddProduct;
