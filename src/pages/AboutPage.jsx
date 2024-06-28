6a9653d05116591d36e11285a26eee443b77ecb2
Bekjan1337 <167067494+Bekjan1337@users.noreply.github.com>
2024-06-28 15:05:02 +0600

    Update AboutPage.jsx

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,7 +1,157 @@
 import React from "react";
-
+import "./AboutPage.css";
 const AboutPage = () => {
-  return <div></div>;
+  return (
+    <div className="aboutSection">
+      <ul class="results">
+        <li class="result">
+          <a href="#">
+            <img
+              src="https://img.taste.com.au/jUGIU1GJ/taste/2020/07/landmark-pagoda-in-doi-inthanon-national-park-at-chiang-mai-thailand-163355-1.jpg"
+              alt=""
+              width={200}
+              height={200}
+            />
+          </a>
+        </li>
+        <li class="result">
+          <a href="#">
+            <img
+              src="https://res.cloudinary.com/enchanting/q_70,f_auto,w_800,h_500,c_fill,g_face/hj-web/2015/09/man-kayaking-in-lagoon-sea-water-near-Koh-Kra-island-in-Thailand.jpg"
+              alt=""
+              width={200}
+              height={200}
+            />
+          </a>
+        </li>
+        <li class="result">
+          <a href="#">
+            <img
+              src="https://media.timeout.com/images/106015066/750/562/image.jpg"
+              alt=""
+              width={200}
+              height={200}
+            />
+          </a>
+        </li>
+        <li class="result">
+          <a href="#">
+            <img
+              src="https://s.abcnews.com/images/Lifestyle/rome-gty-er-170826_16x9_992.jpg"
+              alt=""
+              width={200}
+              height={200}
+            />
+          </a>
+        </li>
+        <li class="result">
+          <a href="#">
+            <img
+              src="https://www.journalofnomads.com/wp-content/uploads/2019/09/Kel-Suu-Lake-Kyrgyzstan-1024x768.jpg"
+              alt=""
+              width={200}
+              height={200}
+            />
+          </a>
+        </li>
+        <li class="result">
+          <a href="#">
+            <img
+              src="https://cdn.britannica.com/06/171306-050-C88DD752/Aurora-borealis-peninsula-Snaefellsnes-Iceland-March-2013.jpg"
+              alt=""
+              width={200}
+              height={200}
+            />
+          </a>
+        </li>
+        <li class="result">
+          <a href="#">
+            <img
+              src="https://www.heyiceland.is/asset/29414/the-blue-lagoon-adobestock_259489027-large.jpeg"
+              alt=""
+              width={200}
+              height={200}
+            />
+          </a>
+        </li>
+        <li class="result">
+          <a href="#">
+            <img
+              src="https://media.cnn.com/api/v1/images/stellar/prod/230210161917-01-japan-never-traveler-culture-tokyo.jpg?c=16x9&q=h_833,w_1480,c_fill"
+              alt=""
+              width={200}
+              height={200}
+            />
+          </a>
+        </li>
+        <li class="result">
+          <a href="#">
+            <img
+              src="https://www.state.gov/wp-content/uploads/2019/04/Japan-2107x1406.jpg"
+              alt=""
+              width={200}
+              height={200}
+            />
+          </a>
+        </li>
+        <li class="result">
+          <a href="#">
+            <img
+              src="https://cdn.britannica.com/65/162465-050-9CDA9BC9/Alps-Switzerland.jpg"
+              alt=""
+              width={200}
+              height={200}
+            />
+          </a>
+        </li>
+        <li class="result">
+          <a href="#">
+            <img
+              src="https://hips.hearstapps.com/hmg-prod/images/great-ocean-road-174028267-1494616481.jpg"
+              alt=""
+              width={200}
+              height={200}
+            />
+          </a>
+        </li>
+      </ul>
+      <section className="quote">
+        <p>
+          “We live in a wonderful world that is full of beauty, charm, and
+          adventure. There is no end to the adventures we can have if only we
+          seek them with our eyes open.” — Jawaharlal Nehru
+        </p>
+      </section>
+      <h1 className="h1">Our Mission</h1>
+      <h3 className="h3">ExploreXperience - </h3>
+      <p className="p">
+        bring travellers to experience our world in a unique way through
+        unparalleled access and insights into a world steeped in history and
+        atthe crossroads of modernity. Hundreds of tours over two decades
+        continue to affirm how excellent service and organisation attracts
+        people from all around the world to discover these ancient lands, often
+        to return again.
+      </p>
+      <p className="p">
+        Our identity as a company has not changed: we believe sustainable and
+        community-based tourism brings a viable future and livelihood to many
+        here, while people-based experiences of the land and culture are what
+        visitors treasure the most when coming with us. We create unforgettable
+        trips for groups of all sizes with diverse needs.
+      </p>{" "}
+      <p className="p">
+        In addition to customised private tours, we also offer our classic tours
+        that are open to smaller groups or individuals wishing to join regular
+        departures. Our group tours have no more than 16 people on each trip so
+        that the experience remains closely personal and beautifully
+        unforgettable for every individual.{" "}
+      </p>
+      <section className="quote2">
+        "A traveler without observation is a bird without wings." — Moslih Eddin
+        Saadi
+      </section>
+    </div>
+  );
 };
 
 export default AboutPage;
